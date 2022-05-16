c8d08ee0f5a87bfef9a8c48b1d5613c094c40c4b
Yair Julio <yairjulio@gmail.com>
2022-05-15 22:32:52 -0300

    JulioShop V1

---
+++ src/Navbar.js
@@ -0,0 +1,23 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+const Navbar = () =>{
+    return(
+        <nav className="navbar sticky-top navbar-expand-lg navbar-dark bg-dark">
+            <a class="navbar-brand" href="#">JulioShop</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div className="navbar-nav">
+                <a className="nav-item nav-link" href="#">Opcion 1</a>
+                <a className="nav-item nav-link" href="#">Opcion 2</a>
+                <a className="nav-item nav-link" href="#">Opcion 3</a>
+                <a className="nav-item nav-link" href="#">Opcion 4</a>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
